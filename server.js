33aca1855f4142a1027aefa30b0da6eccc016fee
KingMo <macv123321@gmail.com>
2022-08-21 17:28:46 -0400

    added ability to delete friend

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(require('./routes'));
-app.use(require('./routes'));
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
-    useFindAndModify: false,
+mongoose.connect
+    (process.env.MONGODB_URI || 'mongodb://localhost/sociaNetwork', 
+    {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+    },
+);
+
+mongoose.set('debug', process.env.NODE_ENV !== 'production');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
 mongoose.set('debug', true);
 app.listen(PORT, () => console.log(`Connect to localhost:${PORT}`))
